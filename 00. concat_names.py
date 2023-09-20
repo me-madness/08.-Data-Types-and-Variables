c0bc1e2c20bdf293717f516d374508d41255dd74
me_madness <p_rangel@abv.bg>
2023-09-20 18:47:24 +0200

    lab for today

---
+++ 00. concat_names.py
@@ -0,0 +1,10 @@
+# some_string = "AAAMMMDDKKRRAAAIIII"
+# print(set(some_string))
+
+# # List
+# [1,2,3,4,5,6[1,2,3,4,5,6]]
+
+# # Dictionary
+# {1: "Petar", "name": "Ivan", 3: "Atanas"}
+
+
--- concat_names.py
+++
@@ -1,8 +0,0 @@
-some_string = "AAAMMMDDKKRRAAAIIII"
-print(set(some_string))
-
-# List
-[1,2,3,4,5,6[1,2,3,4,5,6]]
-
-# Dictionary
-{1: "Petar", "name": "Ivan", 3: "Atanas"}
